fc3d231277cb01bfaa3b2ffea825671b4a12e7b7
Ionică Bizău <bizauionica@yahoo.com>
2015-02-22 11:10:31 +0200

    Added arguments feature. Cleanup.

--- test/index.js
+++ test/index.js
@@ -2,16 +2,18 @@
 var Limiter = require("../lib");
 
 // Create the limiter
-var l = new Limiter(10);
+var l = new Limiter(10)
+  , c = 1
+  ;
 
 // Add 20 functions, running them in parallel but not more than 10 same time
-new Array(20).join("1").split("").forEach(function (_, i) {
-    l.add(function (callback) {
+new Array(30).join("1").split("").forEach(function (_, i) {
+    l.add(function (count, callback) {
         var random = Math.random() * 1000;
         setTimeout(function() {
-            callback(random);
+            callback(random, i);
         }, random);
-    }, function (rand) {
-        console.log("> Function %s done. Random number is: %s", i, rand);
+    }, [i], function (rand, i) {
+        console.log("> %s: Function %s done. Random number is: %s", ++c, i, rand);
     });
 });
